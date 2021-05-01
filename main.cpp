45695f8ddd90ea4de6af604c7cf6414079dc61c4
Andrey Zharkov <andrey.zharkov@phystech.edu>
2021-05-02 00:12:35 +0300

    better random

--- main.cpp
+++ main.cpp
@@ -1,8 +1,7 @@
-#include <iostream>
 #include "MazeGenerator.h"
 
 int main() {
-    std::vector<std::vector<int>> maze = GetMaze(30, 30);
+    std::vector<std::vector<int>> maze = GetMaze(69, 69);
     PrintMaze(maze);
     return 0;
 }
