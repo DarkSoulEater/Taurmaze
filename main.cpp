118d85c7a0d7d62a31d3c658df4f1e7451550442
Shuringan <bygaga.er@yandex.ru>
2021-05-02 00:22:39 +0300

    better random

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include "MazeGenerator.h"
 
 int main() {
-    std::vector<std::vector<int>> maze = GetMaze(69, 69);
+    std::vector<std::vector<int>> maze = GetMaze(17, 17);
     PrintMaze(maze);
     return 0;
 }
